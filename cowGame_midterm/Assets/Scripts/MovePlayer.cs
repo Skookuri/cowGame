621d8442c1ae0f9b252ef248d8b55f59643bd57a
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2024-09-28 15:25:00 -0400

    Added Stand-in Art and Movement Script Change
    Added stand-in art for cow (platypus png) and player art (Jacob pointing) and changed Player movement script to match the player top-down tutorial script

--- cowGame_midterm/Assets/Scripts/MovePlayer.cs
+++ cowGame_midterm/Assets/Scripts/MovePlayer.cs
@@ -1,43 +1,97 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
-public class MoveSprite : MonoBehaviour
-{
-    // Speed at which the sprite moves
-    public float moveSpeed = 1f;
+public class PlayerMoveAround : MonoBehaviour {
 
-    // Variable to store the initial scale of the sprite
-    private Vector3 originalScale;
+      //public Animator anim;
+      //public AudioSource WalkSFX;
+      public Rigidbody2D rb2D;
+      private bool FaceRight = true; // determine which way player is facing.
+      public static float runSpeed = 10f;
+      public float startSpeed = 10f;
+      public bool isAlive = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Save the original scale of the sprite at the start
-        originalScale = transform.localScale;
-    }
+      void Start(){
+           //anim = gameObject.GetComponentInChildren<Animator>();
+           rb2D = transform.GetComponent<Rigidbody2D>();
+      }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Check if the "A" key is held down
-        if (Input.GetKey(KeyCode.A))
-        {
-            // Move the sprite exactly by 1 unit per frame in the left direction
-            transform.Translate(Vector3.left * moveSpeed);
+      void Update(){
+            //NOTE: Horizontal axis: [a] / left arrow is -1, [d] / right arrow is 1
+            //NOTE: Vertical axis: [w] / up arrow, [s] / down arrow
+            Vector3 hvMove = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+           if (isAlive == true){
+                  transform.position = transform.position + hvMove * runSpeed * Time.deltaTime;
 
-            // Flip the sprite by setting the localScale's x to negative
-            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
-        }
+                  if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0)){
+                  //     anim.SetBool ("Walk", true);
+                  //     if (!WalkSFX.isPlaying){
+                  //           WalkSFX.Play();
+                  //     }
+                  } else {
+                  //     anim.SetBool ("Walk", false);
+                  //     WalkSFX.Stop();
+                 }
 
-        // Check if the "D" key is held down
-        if (Input.GetKey(KeyCode.D))
-        {
-            // Move the sprite exactly by 1 unit per frame in the right direction
-            transform.Translate(Vector3.right * moveSpeed);
+                  // Turning. Reverse if input is moving the Player right and Player faces left.
+                 if ((hvMove.x <0 && !FaceRight) || (hvMove.x >0 && FaceRight)){
+                        playerTurn();
+                  }
+            }
+      }
 
-            // Restore the sprite's original scale to face right
-            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
-        }
-    }
-} //comment
+      private void playerTurn(){
+            // NOTE: Switch player facing label
+            FaceRight = !FaceRight;
+
+            // NOTE: Multiply player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+      }
+}
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class MoveSprite : MonoBehaviour
+// {
+//     // Speed at which the sprite moves
+//     public float moveSpeed = 1f;
+
+//     // Variable to store the initial scale of the sprite
+//     private Vector3 originalScale;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         // Save the original scale of the sprite at the start
+//         originalScale = transform.localScale;
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // Check if the "A" key is held down
+//         if (Input.GetKey(KeyCode.A))
+//         {
+//             // Move the sprite exactly by 1 unit per frame in the left direction
+//             transform.Translate(Vector3.left * moveSpeed);
+
+//             // Flip the sprite by setting the localScale's x to negative
+//             transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+//         }
+
+//         // Check if the "D" key is held down
+//         if (Input.GetKey(KeyCode.D))
+//         {
+//             // Move the sprite exactly by 1 unit per frame in the right direction
+//             transform.Translate(Vector3.right * moveSpeed);
+
+//             // Restore the sprite's original scale to face right
+//             transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+//         }
+//     }
+// } //comment
