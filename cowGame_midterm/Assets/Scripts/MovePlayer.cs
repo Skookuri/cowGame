dd1d63d90ca520ad712c48734770dbc59cc30319
am3li300 <amelie.hopke@tufts.edu>
2024-10-01 19:05:39 -0400

    changed ui and made player movement smoother

--- cowGame_midterm/Assets/Scripts/MovePlayer.cs
+++ cowGame_midterm/Assets/Scripts/MovePlayer.cs
@@ -46,9 +46,9 @@ public class PlayerMoveAround : MonoBehaviour {
             FaceRight = !FaceRight;
 
             // NOTE: Multiply player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
+            //Vector3 theScale = transform.localScale;
+            //theScale.x *= -1;
+            //transform.localScale = theScale;
       }
 }
 
