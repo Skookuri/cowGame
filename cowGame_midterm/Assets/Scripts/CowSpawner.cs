734bae549ab938f394b567137c6db494e6a75362
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2024-10-08 01:29:35 -0400

    Added random cow movement, continuous spawning of cows, and time between cow spawning

--- cowGame_midterm/Assets/Scripts/CowSpawner.cs
+++ cowGame_midterm/Assets/Scripts/CowSpawner.cs
@@ -78,17 +78,42 @@ public class CowSpawner: MonoBehaviour
     public int numberToSpawn;
     public List<GameObject> spawnPool;
     // public GameObject spawnArea;
+    // public GameHandler timer;
+    private float waitCounter;
+    public float waitTime;
+    private bool timerIsRunning;
+    private int numberSpawned;
+    private GameObject player;
 
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
+        waitCounter = waitTime;
         spawnObjects();
 
+    }
+
+    void Update()
+    {
+        waitCounter -= Time.deltaTime;
+
+        // Debug.Log("Wait time: " + waitTime);
+        // Debug.Log("Wait Counter: " + waitCounter);
+        if (waitCounter <= 0)
+        {
+            spawnObjects();
+            waitCounter = waitTime;
+        }
+        // timerIsRunning = timer.GetComponent<timerIsRunning>();
+        // while (timerIsRunning)
+        // {
+        //     spawnObjects();
+        // }
     }
 
     public void spawnObjects()
     {
         int randomItem = 0;
-        float spawnDelay = 0;
         GameObject toSpawn;
         // MeshCollider c = spawnArea.GetComponent<MeshCollider>();
         // Debug.Log("Min x: " + c.bounds.min.x);
@@ -98,24 +123,30 @@ public class CowSpawner: MonoBehaviour
 
         float screenX, screenY;
         Vector2 pos;
+
+        randomItem = Random.Range(0,spawnPool.Count);
+        toSpawn = spawnPool[randomItem];
+
+        screenX = Random.Range(-6, 7);
+        screenY = Random.Range(-4, 4);
+
+        pos = new Vector2(screenX, screenY);
+
+        Instantiate(toSpawn, pos,toSpawn.transform.rotation); 
+        numberSpawned++;
         
-        for(int i = 0; i < numberToSpawn; i++)
-        {
-            randomItem = Random.Range(0,spawnPool.Count);
-            toSpawn = spawnPool[randomItem];
+        // for(int i = 0; i < numberToSpawn; i++)
+        // {
+        //     randomItem = Random.Range(0,spawnPool.Count);
+        //     toSpawn = spawnPool[randomItem];
 
-            screenX = Random.Range(-6, 7);
-            screenY = Random.Range(-4, 4);
+        //     screenX = Random.Range(-6, 7);
+        //     screenY = Random.Range(-4, 4);
 
-            // screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
-            // Debug.Log("screenX: " + screenX);
-            // screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
-            // Debug.Log("screenY: " + screenY);
+        //     pos = new Vector2(screenX, screenY);
 
-            pos = new Vector2(screenX, screenY);
-
-            Instantiate(toSpawn, pos,toSpawn.transform.rotation);
-        }
+        //     Instantiate(toSpawn, pos,toSpawn.transform.rotation);
+        // }
     }
 
     private void destroyObjects()
--- cowGame_midterm/Assets/Scripts/MoveCow.cs
+++ cowGame_midterm/Assets/Scripts/MoveCow.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class MoveCow : MonoBehaviour
 {
+    private Rigidbody2D cowRigidBody;
+
     private GameObject player;
     public float speed;
 
     private float distance;
+
+    public bool isWalking;
+
+    public float walkTime;
+    private float walkCounter;
+    public float waitTime;
+    private float waitCounter;
+
+    private int walkDirection;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        cowRigidBody = GetComponent<Rigidbody2D>();
+
+        waitCounter = waitTime;
+        walkCounter = walkTime;
+        ChooseDirection();
     }
 
     // Update is called once per frame
@@ -24,10 +41,58 @@ public class MoveCow : MonoBehaviour
 
         if (distance < 5)
         {
+            isWalking = true;
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, -1 * speed * Time.deltaTime);
             // transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
+        else
+        {
+            if(isWalking)
+            {
+                walkCounter -= Time.deltaTime;
+
+                switch(walkDirection)
+                    {
+                        case 0:
+                            cowRigidBody.velocity = new Vector2(0, speed);
+                            break;
+                        case 1:
+                            cowRigidBody.velocity = new Vector2(speed, 0);
+                            break;
+                        case 2:
+                            cowRigidBody.velocity = new Vector2(0, -speed);
+                            break;
+                        case 3:
+                            cowRigidBody.velocity = new Vector2(-speed, 0);
+                            break;
+                    }
+
+                if (walkCounter < 0)
+                {
+                    isWalking = false;
+                    waitCounter = waitTime;
+                }
+            }
+            else
+            {
+                waitCounter -= Time.deltaTime;
+                cowRigidBody.velocity = Vector2.zero;
+                if (waitCounter < 0)
+                {
+                    ChooseDirection();
+                }
+        }
         
+        }
+
+        
+    }
+
+    public void ChooseDirection()
+    {
+        walkDirection = Random.Range(0, 4);
+        isWalking = true;
+        walkCounter = walkTime;
     }
 
     // private void OnCollisionEnter2D(Collision2D collision)
