c0e82fa335b202e5888d3effaf516a8d3c911671
Skookuri <alyssa.williams.tk360@gmail.com>
2024-10-14 18:50:39 -0400

    Added/Implemented Animations for Cow Colors. Fixed transparency with Black Cows

--- cowGame_midterm/Assets/Scripts/CowSpawner.cs
+++ cowGame_midterm/Assets/Scripts/CowSpawner.cs
@@ -64,30 +64,39 @@ public class CowSpawner: MonoBehaviour
         GameObject spawnedCow = Instantiate(toSpawn, pos,toSpawn.transform.rotation); 
         numberSpawned++;
 
-        // Transform cow1 = spawnedCow.transform.Find("cow1"); //get cow1 info for Cow
-        // Animator cowAnimator = cow1.gameObject.AddComponent<Animator>(); //added Animator component to cow1
+        bool isBlack = false;
+        bool isBrown = false;
+        bool isPink = false;
 
-        // //set Cow_Controller as Animator Controller
-        // RuntimeAnimatorController cowController = Resources.Load<RuntimeAnimatorController>("Cow_Controller");
-        // if (cowController == null) {
-        //     Debug.LogError("Cow_Controller could not be found in the Resources folder!");
-        // } else {
-        //     cowAnimator.runtimeAnimatorController = cowController;
-        // }
+        Transform cowTransform = null;
 
-        
-        // for(int i = 0; i < numberToSpawn; i++)
-        // {
-        //     randomItem = Random.Range(0,spawnPool.Count);
-        //     toSpawn = spawnPool[randomItem];
+        if (spawnedCow.name == "Cow 1(Clone)") {
+            isBlack = true;
+            cowTransform = spawnedCow.transform.Find("cow1"); //get cow1 info for Black Cow
+        } else if (spawnedCow.name == "Cow2(Clone)"){
+            isBrown = true;
+            cowTransform = spawnedCow.transform.Find("cow2"); //get cow2 info for Brown Cow
+        } else if (spawnedCow.name == "Cow3(Clone)") {
+            isPink = true;
+            cowTransform = spawnedCow.transform.Find("cow3"); //get cow3 info for Pink Cow
+        }
 
-        //     screenX = Random.Range(-6, 7);
-        //     screenY = Random.Range(-4, 4);
+        Animator cowAnimator = cowTransform.gameObject.AddComponent<Animator>(); //added Animator component to cow1/cow2/cow3
 
-        //     pos = new Vector2(screenX, screenY);
+        //set Cow_Controller as Animator Controller
+        RuntimeAnimatorController cowController = Resources.Load<RuntimeAnimatorController>("Cow_Controller");
 
-        //     Instantiate(toSpawn, pos,toSpawn.transform.rotation);
-        // }
+        if (cowController == null) {
+            Debug.LogError("Cow_Controller could not be found in the Resources folder!");
+        } else if (cowTransform != null) {
+            cowAnimator.runtimeAnimatorController = cowController;
+            // Set the bool values for the Animator
+            cowAnimator.SetBool("isBlack", isBlack);   // SEND BOOL INFO TO ANIMATOR
+            cowAnimator.SetBool("isBrown", isBrown);   // SEND BOOL INFO TO ANIMATOR
+            cowAnimator.SetBool("isPink", isPink);     // SEND BOOL INFO TO ANIMATOR
+        } else {
+            Debug.LogError("Cow transform not found for the spawned cow: " + spawnedCow.name);
+        }
     }
 
     private void destroyObjects()
